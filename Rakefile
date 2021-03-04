e4bf89ffd66dd12f6c93f5c5df0cde477de2365c
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2021-03-03 21:33:58 -0500

    remote debug working

--- Rakefile
+++ Rakefile
@@ -1,5 +1,11 @@
 task default: %w[test]
 
-task :test do
+task :test => %w[minitest test_unit]
+
+task :test_unit do
+  ruby "src/testunit_example.rb"
+end
+
+task :minitest do
   ruby "src/minitest_examples.rb"
 end
--- src/testunit_example.rb
+++ src/testunit_example.rb
@@ -1,7 +1,70 @@
 # require "test/unit/assertions"
+# nested test case example.
+require 'test/unit'
 
-include Test::Unit::Assertions
+class MyTest < Test::Unit::TestCase
 
-hello = 'world'
+  # include Test::Unit::Data
+  #
 
-assert_equal 'wosrld', hello, "hello function should return 'world'"
+  def setup
+    puts "setup test"
+  end
+
+  def teardown
+    puts "teardown test"
+  end
+
+  class << self
+    def startup
+      puts "Starting up test_unit"
+    end
+
+    def shutdown
+      puts "Shutting down test_unit"
+    end
+  end
+
+  def test_fail
+    assert(false, 'Assertion was false.')
+  end
+
+  def test_xyz
+    x = 5
+    y = x + 3
+    assert_equal({ a: 1, b: 2 }, 4, 'Assertion was false.')
+  end
+  #
+  #
+
+  # def test_empty?(data)
+  #   puts "TESTING"
+  #   puts data
+  #   # assert_equal(data["expected"], data["target"].empty?)
+  # end
+
+  # base_dir = File.dirname(__FILE__)
+  # file_name = 'sample.csv'
+  # fixture_file_path = File.join(base_dir, '..', "fixtures", file_name)
+  # load_data(fixture_file_path)
+  # #todo: seems that if there is data, it will just call any method starting with 'test' over and over with each row. how?
+  # #
+  # def testabc(data)
+  #   puts "TESTING not empty"
+  #   x = 5 + 5
+  #   y = x + 5
+  #   puts data
+  #   # assert_equal(data["expected"], data["target"].empty?)
+  # end
+
+  # def test_fixtures
+  #   base_dir = File.dirname(__FILE__)
+  #   file_name = 'sample.csv'
+  #   fixture_file_path = File.join(base_dir, '..', "fixtures", file_name)
+  #   self.class.load_data(fixture_file_path)
+  #   puts @internal_data.test_data
+  #   puts @internal_data.test_data_label
+  #
+  # end
+end
+
